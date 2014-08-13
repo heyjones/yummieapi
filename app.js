11ba4524cf835e7edc79d282ff1bb30df8116644
heyjones <chris@heyjones.com>
2014-08-13 06:21:52 -0700

    testing webhook

--- app.js
+++ app.js
@@ -14,17 +14,63 @@ app.get('/', function(req, res){
 });
 
 app.post('/shopify/order/new', function(req, res){
-/* console.log(req.body.id); */
+
+	var orderNo = 0;
+
 	var Products = new Array();
-	var i;
 	for(line_item in req.body.line_items){
 		var Product = new Object();
 		Product.barcode = req.body.line_items[line_item].sku;
 		Product.qty = req.body.line_items[line_item].quantity;
 		Products.push(Product);
 	}
-console.log(Products);
-	res.send('done');
+
+	var Person = new Object();
+	Person.firstName = req.body.customer.first_name;
+	Person.lastName = req.body.customer.last_name;
+	Person.address = req.body.shipping_address.address1;
+	Person.postcode = req.body.shipping_address.zip;
+	Person.suburb = req.body.shipping_address.city;
+	Person.state = req.body.shipping_address.province_code;
+	Person.email = req.body.email;
+	Person.phone = req.body.shipping_address.phone;
+	Person.optInMailingList = req.body.buyer_accepts_marketing;
+	Person.countryCodeISO3166_A2 = req.body.customer.country_code;
+
+	var Order = new Object();
+	Order.products = Products;
+	Order.person = Person;
+	Order.refNo = req.body.id;
+
+	var data = JSON.stringify({order: Order});
+	var headers = {
+		'Content-Type': 'application/json',
+		'Content-Length': data.length
+	};
+	var options = {
+		host: 'dev8.nicheweb.com.au',
+		port: 80,
+		path: '/feed.asmx/CreateOrderTest',
+		method: 'POST',
+		headers: headers
+	};
+	var req = http.request(options, function(r){
+		r.setEncoding('utf-8');
+		r.on('data', function(data){
+			orderNo = data;
+		});
+		r.on('end', function(){
+			res.send(orderNo);
+		});
+	});
+	req.on('error', function(e){
+		console.log(e)
+	});
+	req.write(data);
+	req.end();
+
+	res.send(orderNo);
+
 });
 
 app.get('/shopify/orders.json', function(req, res){
